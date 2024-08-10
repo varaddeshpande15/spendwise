2125263eb1125012170aa4326a45ab70700f017a
tesapvg <varaddeshpande81@gmail.com>
2024-08-10 23:06:25 +0530

    Created a Next App

---
+++ spendwise/app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className=" w-full h-full mt-64 flex flex-col items-center justify-center">
+      <h1 className="font-semibold font-sans text-8xl">Hello World</h1>
+      <p className="font-normal font-serif text-4xl">I am building something....</p>
+    </div>
+  );
+}
